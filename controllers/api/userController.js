3eba62d21977bd85e8021fd7319cf4f399e59df3
Faiznurfaza <faiznurfaza11@gmail.com>
2023-08-08 14:19:20 +0700

    Add Register function to userController

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,6 +1,42 @@
 const { User } = require("../../models");
 
 module.exports = {
+  register: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const existingUser = await User.findOne({
+        where: {
+          password: password
+        }
+      })
+
+      if(existingUser) return res.status(409).json({
+        status: "Error",
+        message: "User already registered"
+      })
+
+      const newUser = await User.create({
+        username: username,
+        password: password,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      return res.status(201).json({
+        status: "Success",
+        message: "Register success",
+        data: newUser
+      })
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: "Error",
+        message: "Internal Server Error",
+        error: error,
+      });
+    }
+  },
+
   login: async (req, res) => {
     try {
       const password = req.body.password;
@@ -8,30 +44,31 @@ module.exports = {
         where: { password: password },
       });
 
-      if(!existingUser) return res.status(404).json({
-        status: "Error",
-        message: "User not found"
-      })
+      if (!existingUser)
+        return res.status(404).json({
+          status: "Error",
+          message: "User not found",
+        });
 
-      req.session.userid = existingUser.id
+      req.session.userid = existingUser.id;
 
       return res.status(200).json({
         status: "Success",
         message: "Login Success",
-      })
+      });
     } catch (error) {
       console.log(error);
     }
   },
-  logout: async(req, res) => {
+  logout: async (req, res) => {
     try {
-        req.session.destroy()
-        res.json({
-            status: "Success",
-            message: "Logout success"
-        })
+      req.session.destroy();
+      res.json({
+        status: "Success",
+        message: "Logout success",
+      });
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-  }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const userController = require('../../controllers/api/userController')
 
 router.post('/login', userController.login)
+router.post('/register', userController.register)
 router.post('/logout', userController.logout)
 
 module.exports = router
